63ec41ae35278602ffe5f75be6fbbfff36577ac0
speti43 <speti43@gmail.com>
2018-12-04 10:18:03 +0100

    no special case for filterstate

--- src/clr-angular/data/datagrid/providers/state.provider.ts
+++ src/clr-angular/data/datagrid/providers/state.provider.ts
@@ -24,105 +24,94 @@ import { SerializableFilter } from '../interfaces/serializable.filter.interface'
  */
 @Injectable()
 export class StateProvider<T> {
-  constructor(
-    private filters: FiltersProvider<T>,
-    private sort: Sort<T>,
-    private page: Page,
-    private debouncer: StateDebouncer
-  ) {}
+    constructor(
+        private filters: FiltersProvider<T>,
+        private sort: Sort<T>,
+        private page: Page,
+        private debouncer: StateDebouncer
+    ) { }
 
-  /**
-   * The Observable that lets other classes subscribe to global state changes
-   */
-  change: Observable<ClrDatagridStateInterface<T>> = this.debouncer.change.pipe(map(() => this.state));
-
-  /*
-     * By making this a getter, we open the possibility for a setter in the future.
-     * It's been requested a couple times.
+    /**
+     * The Observable that lets other classes subscribe to global state changes
      */
-  get state(): ClrDatagridStateInterface<T> {
-    const state: ClrDatagridStateInterface<T> = {};
-    if (this.page.size > 0) {
-      state.page = { from: this.page.firstItem, to: this.page.lastItem, size: this.page.size };
-    }
-    if (this.sort.comparator) {
-      if (this.sort.comparator instanceof DatagridPropertyComparator) {
-        /*
-                 * Special case for the default object property comparator,
-                 * we give the property name instead of the actual comparator.
-                 */
-        state.sort = { by: (<DatagridPropertyComparator<T>>this.sort.comparator).prop, reverse: this.sort.reverse };
-      } else {
-        state.sort = { by: this.sort.comparator, reverse: this.sort.reverse };
-      }
-    }
+    change: Observable<ClrDatagridStateInterface<T>> = this.debouncer.change.pipe(map(() => this.state));
 
-    const activeFilters = this.filters.getActiveFilters();
-    if (activeFilters.length > 0) {
-      state.filters = [];
-      for (const filter of activeFilters) {
-        if (filter instanceof DatagridStringFilterImpl) {
-          const stringFilter = filter.filterFn;
-          if (stringFilter instanceof DatagridPropertyStringFilter) {
-            /*
-                         * Special case again for the default object property filter,
-                         * we give the property name instead of the full filter object.
+    /*
+       * By making this a getter, we open the possibility for a setter in the future.
+       * It's been requested a couple times.
+       */
+    get state(): ClrDatagridStateInterface<T> {
+        const state: ClrDatagridStateInterface<T> = {};
+        if (this.page.size > 0) {
+            state.page = { from: this.page.firstItem, to: this.page.lastItem, size: this.page.size };
+        }
+        if (this.sort.comparator) {
+            if (this.sort.comparator instanceof DatagridPropertyComparator) {
+                /*
+                         * Special case for the default object property comparator,
+                         * we give the property name instead of the actual comparator.
                          */
-            state.filters.push({
-              property: stringFilter.prop,
-              value: filter.value,
-            });
-            continue;
-          }
+                state.sort = { by: (<DatagridPropertyComparator<T>>this.sort.comparator).prop, reverse: this.sort.reverse };
+            } else {
+                state.sort = { by: this.sort.comparator, reverse: this.sort.reverse };
+            }
         }
-        state.filters.push(filter);
-      }
-    }
-    return state;
-  }
 
-  set state(state: ClrDatagridStateInterface<T>) {
-    this.debouncer.changeStart();
-    if (state.page) {
-      this.page.size = state.page.size;
-      this.page.current = Math.ceil(state.page.from / state.page.size) + 1;
+        const activeFilters = this.filters.getActiveFilters();
+        if (activeFilters.length > 0) {
+            state.filters = [];
+            for (const filter of activeFilters) {
+                state.filters.push({
+                    property: filter.filterState.property,
+                    value: filter.filterState.value,
+                });
+            }
+        }
+        return state;
     }
-    if (state.sort) {
-      if (typeof state.sort.by === 'string') {
-        if (!(this.sort.comparator instanceof DatagridPropertyComparator)) {
-          this.sort.comparator = new DatagridPropertyComparator(state.sort.by);
+
+    set state(state: ClrDatagridStateInterface<T>) {
+        this.debouncer.changeStart();
+        if (state.page) {
+            this.page.size = state.page.size;
+            this.page.current = Math.ceil(state.page.from / state.page.size) + 1;
         }
-        if (this.sort.reverse !== state.sort.reverse) {
-          this.sort.toggle(this.sort.comparator);
+        if (state.sort) {
+            if (typeof state.sort.by === 'string') {
+                if (!(this.sort.comparator instanceof DatagridPropertyComparator)) {
+                    this.sort.comparator = new DatagridPropertyComparator(state.sort.by);
+                }
+                if (this.sort.reverse !== state.sort.reverse) {
+                    this.sort.toggle(this.sort.comparator);
+                }
+            } else {
+                this.sort.comparator = state.sort.by;
+                this.sort.reverse = state.sort.reverse;
+            }
         }
-      } else {
-        this.sort.comparator = state.sort.by;
-        this.sort.reverse = state.sort.reverse;
-      }
+        if (state.filters) {
+            const activeFilters = this.filters.getActiveFilters();
+            for (const filter of state.filters) {
+                let filterObject: SerializableFilter<T>;
+                if (filter.hasOwnProperty('property') && filter.hasOwnProperty('value')) {
+                    const defaultFilterRepresentation = filter as { property: string; value: string };
+                    const propertyStringFilter = new DatagridPropertyStringFilter(defaultFilterRepresentation.property);
+                    const stringFilter = new DatagridStringFilterImpl(propertyStringFilter);
+                    stringFilter.value = defaultFilterRepresentation.value;
+                    filterObject = stringFilter;
+                } else {
+                    filterObject = filter as SerializableFilter<T>;
+                }
+                const existing = activeFilters.findIndex(value => value.compatibleToState(filterObject.filterState));
+                if (existing !== -1) {
+                    activeFilters.splice(existing, 1);
+                } else {
+                    this.filters.add(filterObject);
+                }
+            }
+            activeFilters.forEach(filter => this.filters.remove(filter));
+        }
+
+        this.debouncer.changeDone();
     }
-    if (state.filters) {
-      const activeFilters = this.filters.getActiveFilters();
-      for (const filter of state.filters) {
-        let filterObject: SerializableFilter<T>;
-        if (filter.hasOwnProperty('property') && filter.hasOwnProperty('value')) {
-          const defaultFilterRepresentation = filter as { property: string; value: string };
-          const propertyStringFilter = new DatagridPropertyStringFilter(defaultFilterRepresentation.property);
-          const stringFilter = new DatagridStringFilterImpl(propertyStringFilter);
-          stringFilter.value = defaultFilterRepresentation.value;
-          filterObject = stringFilter;
-        } else {
-          filterObject = filter as SerializableFilter<T>;
-        }
-        const existing = activeFilters.findIndex(value => value.compatibleToState(filterObject.filterState));
-        if (existing !== -1) {
-          activeFilters.splice(existing, 1);
-        } else {
-          this.filters.add(filterObject);
-        }
-      }
-      activeFilters.forEach(filter => this.filters.remove(filter));
-    }
-    
-    this.debouncer.changeDone();
-  }
 }
